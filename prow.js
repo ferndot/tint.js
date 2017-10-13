f5cb38d658a4f01ff60ff70ceff712e5955d09cd
Joshua Smith <thewanuki@gmail.com>
2017-10-12 18:55:10 -0700

    Remove inline comments that are part of documentation

--- prow.js
+++ prow.js
@@ -1,30 +1,14 @@
 /*
-*                                     _     
-*       ____  _________ _      __    (_)____
-*      / __ \/ ___/ __ \ | /| / /   / / ___/
-*     / /_/ / /  / /_/ / |/ |/ /   / (__  ) 
-*    / .___/_/   \____/|__/|__(_)_/ /____/  
-*   /_/                        /___/        
 *
-*
+*   prow.js
 *   Miscellaneous color tools for the web developer.
-* 
 *   Licensed under the GPLv3. See LICENSE.
-*
 *   Coded with <3 by Joshua Smith
 *
 */
 
 var prow = {
-	convert: {		
-		// Converts hex to sRGB
-		// hex can be:
-		// - "fff"
-		// - "ffffff"
-		// - "#fff"
-		// - "#ffffff"
-		// - "0xffffff"
-		// Returns an array of the form [r,g,b] where r, g, and b are integers between 0 and 255
+	convert: {
 		hexToRGB: function(hex){
 			// Replace "0x" with "#"
 			hex.replace('0x','#');
@@ -39,23 +23,11 @@ var prow = {
 			return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
 		},
 		
-		// Converts sRGB to hex
-		// rgb is an array of the form [r,g,b] where r, g, and b are integers between 0 and 255
-		// Returns a string of the form "#ffffff"
 		RGBToHex: function(rgb) {
 			return "#" + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1);
 		},
 		
-		// Converts sRGB to linear RGB
-		// rgb is an array of the form [r,g,b] where r, g, and b are integers between 0 and 255
-		// Returns an array of the form [r,g,b] where r, g, and b are decimals between 0 and 1
 		RGBToLinearRGB: function(rgb) {
-			// RsRGB = R8bit/255
-			// GsRGB = G8bit/255
-			// BsRGB = B8bit/255
-			// if RsRGB <= 0.03928 then R = RsRGB/12.92 else R = ((RsRGB+0.055)/1.055) ^ 2.4
-			// if GsRGB <= 0.03928 then G = GsRGB/12.92 else G = ((GsRGB+0.055)/1.055) ^ 2.4
-			// if BsRGB <= 0.03928 then B = BsRGB/12.92 else B = ((BsRGB+0.055)/1.055) ^ 2.4
 			var linearRGB = [];
 			for (var i=0;i<rgb.length;i++) {
 				// Normalize color between 0 (black) and 1 (white)
@@ -72,9 +44,6 @@ var prow = {
 		}
 	},
 	
-	// Returns a decimal value representing the relative luminance (http://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef)
-	// of the given color.
-	// color must be an sRGB array of the form [r,g,b]
 	getRelativeLuminance: function(color) {
 		// Convert sRGB array to linear RGB
 		color = this.convert.RGBToLinearRGB(color)
@@ -85,8 +54,6 @@ var prow = {
 		return relativeLuminance = 0.2126 * color[0] + 0.7152 * color[1] + 0.0722 * color[2];
 	},
 	
-	// Returns the contrast ratio of the two given colors in fractional form.
-	// color1 and color2 must be sRGB arrays of the form [r,g,b]
 	getContrastRatio: function(color1,color2) {	
 		// Get relative luminance of colors
 		var color1Luminance = this.getRelativeLuminance(color1);
